b5abbdc904ce6e78db7d525b8160f110fa298f0b
Elias Hanafi <eliashnf@gmail.com>
2022-10-05 23:19:27 +0200

    Add gitignore file

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -2,10 +2,21 @@ import React from 'react';
 import Card from './components/Card';
 import NavBar from './components/NavBar';
 import { Grid } from '@mui/material';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from './pages/Home';
+import Search from './pages/Search';
 
 function App() {
   return (
     <>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}>
+          <Route index element={<Home />} />
+          <Route path="search" element={<Search />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
     <NavBar/>
     <Card/>
     </>
